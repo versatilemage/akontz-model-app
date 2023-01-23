a70bce93586339a78f01502d6e6a419f052a5e4f
versatilemage <104298679+versatilemage@users.noreply.github.com>
2023-01-23 18:42:49 +0530

    made changes in config and meetings

--- pages/teacher/course/create-meeting.js
+++ pages/teacher/course/create-meeting.js
@@ -1,33 +1,33 @@
 import React, { useRef, useEffect } from 'react';
-import SimplePeer from 'simple-peer';
+// import SimplePeer from 'simple-peer';
 
 function VideoChat() {
-  const localVideoRef = useRef(null);
-  const remoteVideoRef = useRef(null);
+  // const localVideoRef = useRef(null);
+  // const remoteVideoRef = useRef(null);
 
-  useEffect(() => {
-    navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(stream => {
-      localVideoRef.current.srcObject = stream;
+  // useEffect(() => {
+    // navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(stream => {
+    //   localVideoRef.current.srcObject = stream;
 
-      const peer = new SimplePeer({ initiator: true, stream });
+    //   const peer = new SimplePeer({ initiator: true, stream });
 
-      peer.on('signal', data => {
-        // Send the signal data to the remote peer
-      });
+    //   peer.on('signal', data => {
+    //     // Send the signal data to the remote peer
+    //   });
 
-      peer.on('stream', stream => {
-        remoteVideoRef.current.srcObject = stream;
-      });
-      // socket.on('signal', data => {
-      //   peer.signal(data);
-      // });
-    });
-  }, []);
+    //   peer.on('stream', stream => {
+    //     remoteVideoRef.current.srcObject = stream;
+    //   });
+    //   // socket.on('signal', data => {
+    //   //   peer.signal(data);
+    //   // });
+    // });
+  // }, []);
 
   return (
     <div>
-      <video ref={localVideoRef} autoPlay className="identifier-1-for-video"></video>
-      <video ref={remoteVideoRef} autoPlay className="identifier-2-for-video"/>
+      {/* <video ref={localVideoRef} autoPlay className="identifier-1-for-video"></video>
+      <video ref={remoteVideoRef} autoPlay className="identifier-2-for-video"/> */}
     </div>
   );
 }
