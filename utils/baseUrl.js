ec7295de4f32cf6235935a43d3fc04a01477bc85
versatilemage <104298679+versatilemage@users.noreply.github.com>
2023-01-27 10:21:27 +0530

    Update baseUrl.js

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,5 @@
 const baseUrl = process.env.NODE_ENV === "production" 
-? 'https://main.d1zla24hv8trdr.amplifyapp.com' 
-: 'http://localhost:3000';
+? 'http://localhost:3000' 
+: 'https://main.d1zla24hv8trdr.amplifyapp.com';
 
 export default baseUrl;
