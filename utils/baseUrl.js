dc825a6ae4c2d025be73fbc7bd1a9c2d83e8f3e2
versatilemage <104298679+versatilemage@users.noreply.github.com>
2023-01-28 12:12:53 +0530

    Update baseUrl.js

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -2,7 +2,7 @@
 // ? 'http://localhost:3000' 
 // : 'https://main.d1zla24hv8trdr.amplifyapp.com';
 
-const baseUrl = "https://main.d1zla24hv8trdr.amplifyapp.com"
+const baseUrl = "https://main.d1zla24hv8trdr.amplifyapp.com/"
 
 // const baseUrl = "https://akontz-model-app.vercel.app/"
 
